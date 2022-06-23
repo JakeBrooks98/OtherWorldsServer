da27274f8ef04b6d3cd085f5b619809bda7d1c66
Jake Brooks <jacobdbrooks1998@gmail.com>
2022-06-23 13:15:13 -0500

    fixed serializer

--- otherworldsapi/serializers/eventserializer.py
+++ otherworldsapi/serializers/eventserializer.py
@@ -1,15 +1,14 @@
 from otherworldsapi.models import Events
 from rest_framework import serializers
 
-from otherworldsapi.serializers.worldserializers import WorldSerializer
+
 
 class EventsSerializer(serializers.ModelSerializer):
     """JSON serializer for Events
     """
-    world=WorldSerializer()
     class Meta:
         model = Events
-        fields = ('id','description', 'world', 'name', 'date', 'date_suffix')
+        fields = ('id','description', 'name', 'date', 'date_suffix')
         depth = 2
         
 class CreateEventsSerializer(serializers.ModelSerializer):
