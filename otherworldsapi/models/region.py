22804188bc84fd6f9d44ead180c6beeee02b1f58
Jake Brooks <jacobdbrooks1998@gmail.com>
2022-06-23 16:42:46 -0500

    fixed the world model

--- otherworldsapi/models/region.py
+++ otherworldsapi/models/region.py
@@ -4,7 +4,7 @@ from otherworldsapi.models.world import World
 
 class Region(models.Model):
     name = models.CharField(max_length=30)
-    description = models.CharField(max_length=350)
+    description = models.TextField()
     world = models.ForeignKey(
         "World", on_delete=models.CASCADE, related_name='regions')
     biome = models.ManyToManyField( "Biome", related_name='region')
