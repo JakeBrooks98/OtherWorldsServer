c72a5af365933b95a09eb75e2c56f138e6bc38a8
Jake Brooks <jacobdbrooks1998@gmail.com>
2022-06-23 20:25:18 -0500

    updated world model

--- otherworldsapi/models/world.py
+++ otherworldsapi/models/world.py
@@ -6,7 +6,7 @@ class World(models.Model):
     name = models.CharField(max_length=30)
     user = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name='worlds')
-    description = models.CharField(max_length=350)
+    description = models.TextField()
     @property
     def is_user(self):
         return self.__is_user
